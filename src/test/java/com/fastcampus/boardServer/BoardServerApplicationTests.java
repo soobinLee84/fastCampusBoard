404c92990aa23b5fac9cbf0bfb316b9eb91a0d19
soobinLee <sooblair84@gmail.com>
2024-06-09 22:00:33 +0900

    basic settings

---
+++ src/test/java/com/fastcampus/boardServer/BoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.boardServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
